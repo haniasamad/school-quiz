9c0c0350725ac8ede80c009578b816b380be1ef8
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-07-07 12:09:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+var schools = ["shagufata", "orangi children", "matchles", "iqra", "aziz millat", "alhamd"];
+//var school = prompt("school is good")
+var school = prompt("Enter any School Name")
+if(schools[0] === school){alert(schools[0])}
+else if    (schools[1] === school){alert(schools[1])}
+else if    (schools[2] === school){alert(schools[2])}
+else if    (schools[3] === school){alert(schools[3])}
+else if    (schools[4] === school){alert(schools[4])}
+else if    (schools[5] === school){alert(schools[5])}
+else
+alert("this is not our school")
